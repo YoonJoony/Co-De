64b91069c62aaf73c89651bffdc17276efc13fce
dbswn <dbswns1101@naver.com>
2023-07-01 09:05:30 +0900

    commit

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -7,6 +7,19 @@ loginButton.addEventListener("click", () => {
     if (id.value === "user" && pw.value === "1234") {
         alert("로그인 성공");
     } else {
+        alert("아이디 및 비밀번호를 다시 입력해주세요.");
         loginErrorMsg.style.opacity = 1;
     }
+});
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+  container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+  container.classList.remove("right-panel-active");
 });
