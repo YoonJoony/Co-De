f278b44f5171554403c29bf734bc93a0c3223186
YangYunSeok <yangys0928@naver.com>
2023-07-27 22:29:00 +0900

    2023.07.27 22:28 추방투표 추가 및 채팅방 UI 변경

--- Test/Chat/채팅방 V2/chat.js
+++ Test/Chat/채팅방 V2/chat.js
@@ -1,14 +1,14 @@
 function profile_btn() {
-    const box = document.getElementById("layer-header-profile");
-  
-    // btn1 숨기기 (display: none)
-    if (box.style.display !== "block") {
-      box.style.display = "block";
-    }
-    // btn` 보이기 (display: block)
-    else {
-      box.style.display = "none";
-    }
+  const box = document.getElementById("layer-header-profile");
+
+  // btn1 숨기기 (display: none)
+  if (box.style.display !== "block") {
+    box.style.display = "block";
+  }
+  // btn` 보이기 (display: block)
+  else {
+    box.style.display = "none";
+  }
 }
 
 
@@ -17,14 +17,14 @@ const btnClose = document.getElementById('map_close');
 const map_content = document.querySelector('.map');
 
 // 모달창 띄우기
-function mapOpen(){
+function mapOpen() {
   map_content.style.display = 'block';
   btnOpen.style.display = 'none';
   btnClose.style.display = 'block';
 }
 
 // 모달창 닫기
-function mapClose(){
+function mapClose() {
   map_content.style.display = 'none';
   btnOpen.style.display = 'block';
   btnClose.style.display = 'none';
@@ -35,41 +35,62 @@ btnOpen.addEventListener('click', mapOpen);
 btnClose.addEventListener('click', mapClose);
 
 
-$(document).ready(function(){
-  $('#chating').load('chating.html', function(){
-    
+$(document).ready(function () {
+  $('#chating').load('chating.html', function () {
+
   });
 })
 
 // 채팅 관련 js
-$(function(){
-  $("input[type='text']").keypress(function(e){
-      if(e.keyCode == 13 && $(this).val().length){
-          var _val = $(this).val();
-          var _class = $(this).attr("class");
-          $(this).val('');
-          var _tar = $(".chat_wrap .inner").append('<div class="item '+_class+'"><div class="box"><p class="msg">'+_val+'</p><span class="time">'+currentTime()+'</span></div></div>');
+$(function () {
+  $("input[type='text']").keypress(function (e) {
+    if (e.keyCode == 13 && $(this).val().length) {
+      var _val = $(this).val();
+      var _class = $(this).attr("class");
+      $(this).val('');
+      var _tar = $(".chat_wrap .inner").append('<div class="item ' + _class + '"><div class="box"><p class="msg">' + _val + '</p><span class="time">' + currentTime() + '</span></div></div>');
 
-          var lastItem = $(".chat_wrap .inner").find(".item:last");
-          setTimeout(function(){
-              lastItem.addClass("on");
-          },10);
+      var lastItem = $(".chat_wrap .inner").find(".item:last");
+      setTimeout(function () {
+        lastItem.addClass("on");
+      }, 10);
 
-          var position = lastItem.position().top + $(".chat_wrap .inner").scrollTop();
-          console.log(position);
+      var position = lastItem.position().top + $(".chat_wrap .inner").scrollTop();
+      console.log(position);
 
-          $(".chat_wrap .inner").stop().animate({scrollTop:position},500);
-      }
+      $(".chat_wrap .inner").stop().animate({ scrollTop: position }, 500);
+    }
   });
 
 });
 
-var currentTime = function(){
+var currentTime = function () {
   var date = new Date();
   var hh = date.getHours();
   var mm = date.getMinutes();
-  var apm = hh >12 ? "오후":"오전";
-  var ct = apm + " "+hh+":"+mm+"";
+  var apm = hh > 12 ? "오후" : "오전";
+  var ct = apm + " " + hh + ":" + mm + "";
   return ct;
 }
+
+// 햄버거 버튼
+const list = document.getElementById("map_button");
+
+function visit_btn() {
+  // 숨기기 (display: none)
+  if (list.style.display !== "block") {
+    list.style.display = "block";
+  }
+  // 보이기 (display: block)
+  else {
+    list.style.display = "none";
+  }
+}
+
+// ----------------------------------------------
+
+// 투표
+
+
+
 // ----------------------------------------------
