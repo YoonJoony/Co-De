db172a9ed87653a5def83efadea7ab595023903f
alstjd <alstjd308@naver.com>
2023-07-27 18:52:14 +0900

    ff

--- src/main/java/backend/codebackend/controller/MozipController.java
+++ src/main/java/backend/codebackend/controller/MozipController.java
@@ -4,8 +4,11 @@ import backend.codebackend.dto.MozipForm;
 import backend.codebackend.service.MozipService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,8 +17,10 @@ public class MozipController {
     private MozipService mozipService;
 
     @GetMapping("/")
-    public String home() {
-        return "main_page";
+    public String list(Model model) {
+        List<MozipForm> mozipFormList = mozipService.getMozipList();
+        model.addAttribute("postList", mozipFormList);
+        return "Mozip/mozipList.html";
     }
 
     @GetMapping("/post")    //모집 글 생성하는 화면
