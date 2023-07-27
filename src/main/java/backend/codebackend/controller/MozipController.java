cf91915b729bf17c28f1ec23b17e6e18551b9d6f
alstjd <alstjd308@naver.com>
2023-07-27 17:06:23 +0900

    ff

--- src/main/java/backend/codebackend/controller/MozipController.java
+++ src/main/java/backend/codebackend/controller/MozipController.java
@@ -16,12 +16,12 @@ public class MozipController {
 
     private MozipService mozipService;
 
-    @GetMapping("/")    //모집 글 생성 후 리스트들을 보여주는 화면
-    public String list(Model model) {
-        List<MozipForm> mozipFormList = mozipService.getMozipList();
-        model.addAttribute("postList", mozipFormList);
-        return "mozip/MozipList.html";
-    }
+//    @GetMapping("/")    //모집 글 생성 후 리스트들을 보여주는 화면
+//    public String list(Model model) {
+//        List<MozipForm> mozipFormList = mozipService.getMozipList();
+//        model.addAttribute("postList", mozipFormList);
+//        return "mozip/MozipList.html";
+//    }
 
     @GetMapping("/post")    //모집 글 생성하는 화면
     public String post() {
