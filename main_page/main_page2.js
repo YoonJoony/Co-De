6003978b0ecf28ced37f67600f64149e3ebf570f
YoonJoony <dbswns1101@naver.com>
2023-08-21 18:28:44 +0900

    commit

--- main_page/main_page2.js
+++ main_page/main_page2.js
@@ -189,7 +189,6 @@ $(function () {
   $(".board").slice(0, 4).css("display", "inline-block"); // 초기갯수
 
   $(window).scroll(function () {
-
     if ($(window).scrollTop() == $(document).height() - $(window).height()) {
       console.log("스크롤바 끝 도달");
 
