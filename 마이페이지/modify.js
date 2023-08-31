60fda9ddb6921fe0d89b5749cf90e24c525b8f86
ndy <dynam99@naver.com>
2023-08-31 17:22:04 +0900

    마이페이지 수정

---
+++ 마이페이지/modify.js
@@ -0,0 +1,27 @@
+// 비밀번호 변경 버튼 클릭 시 수정 form 보이게 함
+function modify_pw_btn() {
+  const box = document.getElementById("modify_password_form");
+
+  // btn1 숨기기 (display: none)
+  if (box.style.display !== "block") {
+    box.style.display = "block";
+  }
+  // btn` 보이기 (display: block)
+  else {
+    box.style.display = "none";
+  }
+}
+
+// 닉네임 변경 버튼 클릭 시 수정 form 보이게 함
+function modify_nickname_btn() {
+  const box = document.getElementById("modify_nickname_form");
+
+  // btn1 숨기기 (display: none)
+  if (box.style.display !== "block") {
+    box.style.display = "block";
+  }
+  // btn` 보이기 (display: block)
+  else {
+    box.style.display = "none";
+  }
+}
