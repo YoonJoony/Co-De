641457af479c5cbc67c2cc3c8118fe5631c56dd1
YoonJoony <dbswns1101@naver.com>
2023-09-03 23:48:51 +0900

    commit

--- src/main/java/backend/codebackend/controller/ChatController.java
+++ src/main/java/backend/codebackend/controller/ChatController.java
@@ -14,7 +14,9 @@ package backend.codebackend.controller;
 import backend.codebackend.domain.ChatUser;
 import backend.codebackend.domain.Mozip;
 import backend.codebackend.dto.ChatDTO;
+import backend.codebackend.repository.ChatRepository;
 import backend.codebackend.repository.MozipRepository;
+import backend.codebackend.service.ChatService;
 import backend.codebackend.service.ChatUserService;
 import backend.codebackend.service.MozipService;
 import lombok.RequiredArgsConstructor;
@@ -42,7 +44,9 @@ public class ChatController {
     private final SimpMessageSendingOperations template;
     private final MozipRepository mozipRepository;
     private final MozipService mozipService;
+    private final ChatService chatService;
     private final ChatUserService chatUserService;
+
     @MessageMapping("/mozip/chat/enterUser") //해당 주소로 메시지가 도착시 메소드 실행
     public void enterUser(@Payload ChatDTO chat, SimpMessageHeaderAccessor headerAccessor) {//Payload : Message전송할 데이터가 담긴 Wrapper Class로 메시지 요청 시 instance화 된 Message를 생성한다. 그것을 ChatDTO 와 매핑시켜줌
     /*  @Payload : 메시지 핸들러에서 메시지를 처리할 때, 메시지의 payload를 추출하여 메서드 인자로 전달하는 기능
@@ -84,8 +88,8 @@ public class ChatController {
     @MessageMapping("/mozip/chat/sendMessage")
     public void sendMessage(@Payload ChatDTO chat) {
         log.info("CHAT {}", chat);
-        chat.setMessage(chat.getMessage());
         template.convertAndSend("/sub/mozip/chat/room/"+chat.getId(),chat);
+        chatService.save(chat);
     }
 
     @GetMapping("/mozip/chat/userList")
@@ -93,6 +97,7 @@ public class ChatController {
     public ArrayList<String> getUserList(Long id) {
         return chatUserService.getUserList(id);
     }
+
 
     @GetMapping("/mozip/chat/inDuplicateName")
     @ResponseBody
