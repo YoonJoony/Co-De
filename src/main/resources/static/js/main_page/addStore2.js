0b48e8316d398e7474cf3d3a5ac3abf8f57e2a17
YoonJoony <dbswns1101@naver.com>
2023-10-04 15:27:55 +0900

    commit

--- src/main/resources/static/js/main_page/addStore2.js
+++ src/main/resources/static/js/main_page/addStore2.js
@@ -1,7 +1,11 @@
 const search_store_btn = document.querySelector("#add_store");
+const search_store_input_btn = document.querySelector(".storeName-input");
 search_store_btn.addEventListener("click", showStoreList);
+search_store_input_btn.addEventListener("click", showStoreList);
 
 var storeModal_body = document.querySelector(".storeModal_body");
+var loading_div = document.querySelector(".loading-div");
+
 var category;
 var choiceStore = null;
 
@@ -10,11 +14,8 @@ function showStoreList() {
   category = null;
 
   var con = document.querySelector(".storeModal");
-  if (con.style.display == "block") {
-    con.style.display = "none";
-  } else {
-    con.style.display = "block";
-  }
+  con.style.display = "block";
+
 
   selenium();
 }
@@ -23,9 +24,110 @@ function selenium() {
   while(storeModal_body.firstChild) {
     storeModal_body.removeChild(storeModal_body.firstChild);
   }
+
+  loading_div.style.display = "block";
+
     $.ajax({
         type : "GET",
         url : "/mozip/storeList",
+        data : {
+
+        },
+        success: function(data) {
+            for(let i = 0; i < data.length; i++) {
+                var store_image = document.createElement("img");
+                  store_image.classList.add("store_image");
+                  // 백앤드 데이터 가져오기
+                  store_image.setAttribute("src", data[i].imageUrl); // 이미지 소스 추가해야함
+
+                  // store_image_div
+                  var store_image_div = document.createElement("div");
+                  store_image_div.classList.add("store_image_div"); // 클래스 이름 store_image_div으로 설정
+                  store_image_div.appendChild(store_image); // store_info_div의 자식으로 store_images
+
+                  //store_info_div_top div(제목)---------------------------------
+                  var store_info_div_top = document.createElement("div");
+
+                  //제목
+                  var store_name = document.createElement("div");
+                  store_name.classList.add("store_name"); // 클래스 이름 store_name으로 설정
+                  var store_name_text = document.createTextNode(data[i].title); // 크롤링 데이터로 가져온 가게 이름 넣어야함
+                  store_name.appendChild(store_name_text);
+                  store_info_div_top.appendChild(store_name);
+
+                  //store_info_div_bottom div(별점, 리뷰개수...)---------------------------------
+                  var store_info_div_bottom = document.createElement("div");
+                  store_info_div_bottom.classList.add("store_info_div_bottom"); // 클래스 이름 store_name으로 설정
+
+                  //별점
+                  var span = document.createElement("span");
+                  var ico_Star = document.createElement("span");
+                  ico_Star.classList.add("ico-star"); // 클래스 이름 store_image_div으로 설정
+                  var ico_Star_text = document.createTextNode(data[i].icoStar);
+                  ico_Star.appendChild(ico_Star_text);
+                  span.appendChild(ico_Star);
+
+                  //리뷰 개수
+                  var review_num = document.createElement("div");
+                  review_num.classList.add("review_num"); // 클래스 이름 store_image_div으로 설정
+                  var review_num_text = document.createTextNode(data[i].review_num);
+                  review_num.appendChild(review_num_text);
+
+                  // 최소주문금액
+                  var store_minimum_order_amount = document.createElement("div");
+                  store_minimum_order_amount.classList.add("store_minimum_order_amount"); // 클래스 이름 store_minimum_order_amount으로 설정
+                  var minimumOrderData = document.createTextNode(data[i].minPrice); // 크롤링 데이터로 가져온 가게 이름 넣어야함
+                  store_minimum_order_amount.appendChild(minimumOrderData); // store_minimum_order_amount의 자식으로 최소주문가격 데이터
+
+                  store_info_div_bottom.appendChild(span);
+                  store_info_div_bottom.appendChild(review_num);
+                  store_info_div_bottom.appendChild(store_minimum_order_amount);
+
+
+                  // store_info_div
+                  var store_info_div = document.createElement("div");
+                  store_info_div.classList.add("store_info_div"); // 클래스 이름 store_info_div으로 설정
+                  store_info_div.appendChild(store_info_div_top); // store_info_div의 자식으로 store_name
+                  store_info_div.appendChild(store_info_div_bottom); // store_info_div의 자식으로 store_minimum_order_amount
+
+                  //store_time_div
+                  var store_time_div = document.createElement("div");
+                  store_time_div.classList.add("store_time_div");
+                  var store_time = document.createElement("div");
+                  store_time.classList.add("store_time");
+                  var store_time_text = document.createTextNode(data[i].deliveryTime);
+                  store_time.appendChild(store_time_text);
+                  store_time_div.appendChild(store_time);
+
+                  // store_list
+                  var store_list = document.createElement("div");
+                  store_list.classList.add("store_list");
+                  store_list.appendChild(store_image_div);
+                  store_list.appendChild(store_info_div);
+                  store_list.appendChild(store_time_div);
+
+                  loading_div.style.display = "none";
+                  // storeModal_body
+                  storeModal_body.appendChild(store_list);
+              }
+              count = 1;
+        },
+        error: function() {
+            console.log("리스트 요청 실패 : ");
+        }
+    })
+}
+
+
+function selenium_Category() {
+  while(storeModal_body.firstChild) {
+    storeModal_body.removeChild(storeModal_body.firstChild);
+  }
+  loading_div.style.display = "block";
+
+    $.ajax({
+        type : "GET",
+        url : "/mozip/categoryList",
         data : {
           "category" : category
         },
@@ -103,6 +205,7 @@ function selenium() {
                   store_list.appendChild(store_time_div);
 
 
+                  loading_div.style.display = "none";
                   // storeModal_body
                   storeModal_body.appendChild(store_list);
               }
@@ -117,49 +220,49 @@ function selenium() {
 //미개한 방식이니 나중에 클릭 이벤트로 수정하기
 function category_store_list(select_category){
   switch (select_category) {
-    case "전체":
+    case "전체보기":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "프랜차이즈":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "치킨":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "피자/양식":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "중국집":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "한식":
         category = select_category;
-        selenium();
+        selenium_Category();
          break;
     case "일식/돈까스":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "족발/보쌈":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "야식":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "분식":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     case "카페/디저트":
         category = select_category;
-        selenium();
+        selenium_Category();
         break;
     default:
       // 모든 case와 일치하지 않는 경우 실행되는 코드
@@ -176,6 +279,18 @@ function closeStoreList() {
   if (con.style.display == "block") {
     con.style.display = "none";
   }
+   $.ajax({
+          type : "POST",
+          url : "/mozip/closeDriver",
+          data : {
+          },
+          success: function() {
+            console.log("Driver quit()")
+          },
+          error: function() {
+             console.log("Driver 종료 요청 실패 : ");
+          }
+    })
 }
 
 
@@ -274,11 +389,79 @@ scrollContainer.addEventListener('pointermove', (e) => {
     scrollContainer.scrollLeft = scrollLeft - walk;
 });
 
-//문서가 전부 로딩된 후 실행
+//------------------------------모집글 생성 부분------------------------------------------
+var mozipTitle; //제목
+var mozipRange; //거리
+var mozipCategory; //카테고리
+var mozipStore; //가게
+var mozipPeople; //인원 수
+var search_txt = document.querySelector(".search-txt"); //제목 input 요소
+document.querySelector("#modal_close_beck").addEventListener("click", close);
+
+//(아래 함수는 문서가 전부 로딩된 후 실행된다고 함)
 $(function () {
+
+    // -------------글 노출 거리 슬라이더------------
+    let rangeInput = document.getElementById("rangeInput");
+
+    let distances = [0, 100, 300, 500];
+    let selectedDistance;
+
+    rangeInput.addEventListener("input", function(e) {
+        selectedDistance = distances[e.target.value];
+        mozipRange = selectedDistance;
+        console.log(`Selected distance: ${selectedDistance}m`);
+    });
+
+    // -------------선택한 카테고리 이름 불러오기------------
+    $(document).on('click', '.category-item', function() {
+        $(".category-item").css({ "background-color": "#fff", "color": "black" });
+        $(this).css({ "background-color": "#ff8c16", "color": "#FFFFFF" });
+        choiceCategory = $(this).text();
+        mozipCategory = choiceCategory;
+        console.log(choiceCategory);
+    });
+
+    // -------------선택한 가게 이름 불러오기------------
     $(document).on('click', '.store_list', function() {
         choiceStore = $(this).find('.store_name').text();
+        mozipStore = choiceStore;
+        $('#storeName-input').val(choiceStore);
+        closeStoreList();
         console.log(choiceStore);
     });
 });
+
+// -------------모집글 생성 버튼 클릭 시------------
+function createMozip() {
+    //모집글 제목 저장
+    mozipTitle = search_txt.value;
+    //인원 선택한 값 저장
+    mozipPeople = document.getElementById("people").value;
+    console.log(mozipTitle + "///" + mozipRange + "///" + mozipCategory + "///" + mozipStore + "///" + mozipPeople);
+
+    // ----------서버에 모집글 정보 전송------------
+    $.ajax({
+           type : "POST",
+           url : "/mozip",
+           data : {
+                "mozipTitle" : mozipTitle,
+                "mozipRange" : mozipRange,
+                "mozipCategory" : mozipCategory,
+                "mozipStore" : mozipStore,
+                "mozipPeople" : mozipPeople,
+           },
+           success: function() {
+             location.reload();
+             console.log("/mozip 요청 성송")
+           },
+           error: function() {
+              console.log("/mozip 종료 요청 실패 : ");
+           }
+     })
+}
+
+
+
+
 
