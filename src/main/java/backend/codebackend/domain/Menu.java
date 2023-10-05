66ce71efb9cd1c89063091844455abd8c98b5135
YoonJoony <dbswns1101@naver.com>
2023-10-05 16:02:58 +0900

    commit

--- src/main/java/backend/codebackend/domain/Menu.java
+++ src/main/java/backend/codebackend/domain/Menu.java
@@ -15,16 +15,17 @@ public class Menu {
     private String menuDesc;
     private String menuPhoto;
     private final String delivery_fee;
-
+    private final String minPrice;
     private List<List<Menu>> menuList_Title;
     private List<String> menuList_Title_Name;
 
-    public Menu(String menuName, String menuPrice, String menuDesc, String menuPhoto, String delivery_fee, List<List<Menu>> menuList_Title, List<String> menuList_Title_Name) {
+    public Menu(String menuName, String menuPrice, String menuDesc, String menuPhoto, String delivery_fee, String minPrice, List<List<Menu>> menuList_Title, List<String> menuList_Title_Name) {
         this.menuName = menuName;
         this.menuPrice = menuPrice;
         this.menuDesc = menuDesc;
         this.menuPhoto = menuPhoto;
         this.delivery_fee = delivery_fee;
+        this.minPrice = minPrice;
         this.menuList_Title = menuList_Title;
         this.menuList_Title_Name = menuList_Title_Name;
     }
