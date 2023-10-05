653b43227a3921bbb2b5fe3a1d89b607d5821617
YangYunSeok <yangys0928@naver.com>
2023-10-05 22:42:26 +0900

    2023.10.05 양윤석 22:42 게시판페이지 작업중

--- main_page/main_page2.js
+++ main_page/main_page2.js
@@ -285,4 +285,18 @@ $(async function () {
 });
 
 
+function chatbot_open() {
+    const chatbot_content = document.querySelector(".main-right");
+
+    // 숨기기 (display: none)
+    if (chatbot_content.style.display !== "block") {
+        $('html').scrollTop(0);
+        chatbot_content.style.display = "block";
+    }
+    // 보이기 (display: block)
+    else {
+        chatbot_content.style.display = "none";
+    }
+}
+
 // -------------------------------------------------------------------------------------------------------
