21339d45d6ff569086640ff28a09976596e53b6e
NamDongYun <dynam99@naver.com>
2023-10-16 10:30:06 +0900

    마이페이지 주소 설정 추가

--- 프론트앤드/마이페이지/modify.js
+++ 프론트앤드/마이페이지/modify.js
@@ -53,3 +53,39 @@ function modify_profile_image_btn() {
     box.style.display = "none";
   }
 }
+
+// 주소 변경 버튼 클릭 시 수정 form 보이게 함
+function modify_address_btn() {
+  const box = document.getElementById("modify_address_form");
+
+  // btn1 숨기기 (display: none)
+  if (box.style.display !== "block") {
+    box.style.display = "block";
+  }
+  // btn` 보이기 (display: block)
+  else {
+    box.style.display = "none";
+  }
+}
+// 다음 주소
+window.onload = function(){
+  document.getElementById("find_address").addEventListener("click", function(){ //주소입력칸을 클릭하면
+      new daum.Postcode({
+          oncomplete: function(data) { //선택시 입력값 세팅
+              document.getElementById("modified_ad").value = data.address; // 주소 넣기
+              document.querySelector("#modified_ad_detail").focus(); //상세입력 포커싱
+          }
+      }).open();
+  });
+}
+// $(function(){
+//   document.getElementById("find_address").addEventListener("click", function(){ //주소입력칸을 클릭하면
+//       //카카오 지도 발생
+//       new daum.Postcode({
+//           oncomplete: function(data) { //선택시 입력값 세팅
+//               document.getElementById("modified_ad").value = data.address; // 주소 넣기
+//               document.querySelector("modified_ad_detail").focus(); //상세입력 포커싱
+//           }
+//       }).open();
+//   });
+// });
