c17bb26e050b86a8718ca2d6891ddb571b4d682d
YangYunSeok <yangys0928@naver.com>
2023-11-01 20:34:43 +0900

    장바구니창 이동 및 지도 출력

--- Chat_dbswns/새거새거/chat2.js
+++ Chat_dbswns/새거새거/chat2.js
@@ -825,18 +825,10 @@ function detailClose_cos3() {
 const basket_wrap = document.querySelector(".basket-view");
 const basket_header = document.querySelector(".basket-title");
 
-const cal_wrap = document.querySelector(".cal_page");
-const cal_header = document.querySelector(".storeModal_header_topper");
-
 let BlastX = 0;
 let BlastY = 0;
 let BstartX = 0;
 let BstartY = 0;
-
-let ClastX = 0;
-let ClastY = 0;
-let CstartX = 0;
-let CstartY = 0;
 
 // 장바구니 이동
 basket_header.addEventListener('mousedown', function (e) {
@@ -872,37 +864,108 @@ function onMove(e) {
     basket_wrap.style.left = `${basket_wrap.offsetLeft - BlastX}px`;
 }
 
+// 지도 추가
 
-// 결제창
-cal_header.addEventListener('mousedown', function (e) {
-    e.preventDefault();
-    CstartX = e.clientX;
-    CstartY = e.clientY;
+// ---------------------------------------------------------------------------------------------
 
-    // 2.  
-    cal_header.classList.add('active');
+function map_btn() {
+    const map_content = document.getElementById("map");
 
-    // 3.
-    document.addEventListener('mouseup', onRemoveEvent);
-
-    // 4.
-    document.addEventListener('mousemove', onMove);
-});
-
-function onRemoveEvent() {
-    cal_header.classList.remove('active');
-    document.removeEventListener('mouseup', onRemoveEvent);
-    document.removeEventListener('mousemove', onMove);
+    // 숨기기 (display: none)
+    if (map_content.style.display !== "block") {
+        map_content.style.display = "block";
+    }
+    // 보이기 (display: block)
+    else {
+        map_content.style.display = "none";
+    }
 }
 
-function onMove(e) {
-    e.preventDefault();
-    ClastX = CstartX - e.clientX;
-    ClastY = CstartY - e.clientY;
+$(function () {
 
-    CstartX = e.clientX;
-    CstartY = e.clientY;
+    initMap2();
+});
 
-    cal_wrap.style.top = `${cal_wrap.offsetTop - ClastY}px`;
-    cal_wrap.style.left = `${cal_wrap.offsetLeft - ClastX}px`;
+
+
+function initMap2() {
+    // 호스트 좌표 변수
+    let main_lat, main_lng;
+    main_lat = 37.64359950713993;
+    main_lng = 127.02755816582702;
+    const name = "꾸브라꼬";
+
+    var areaArr = new Array();  // 지역을 담는 배열 ( 지역명/위도경도 )
+    areaArr.push(
+        /*이름*/			/*위도*/					/*경도*/
+        { location: name, lat: main_lat, lng: main_lng },  // 호스트 좌표
+    );
+
+
+    let markers = new Array(); // 마커 정보를 담는 배열
+    let infoWindows = new Array(); // 정보창을 담는 배열
+
+
+    // v3 버전 지도 생성
+    var map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(37.64359950713993, 127.02755816582702),
+        zoom: 15,
+        logoControl: false,
+        mapDataControl: false,
+        scaleControl: false,
+        mapTypeControl: false,
+    });
+
+
+    let Slat = 0, Slng = 0;
+
+    for (var i = 0; i < areaArr.length; i++) {
+        Slat += Number(areaArr[i].lat);
+        Slng += Number(areaArr[i].lng);
+    }
+
+    // 중간지점 마커
+    var middlemarker_option = {
+        map: map,
+        position: new naver.maps.LatLng(main_lat, main_lng), // 사용자의 위도 경도 넣기
+        title: '중간지점',
+        icon: {
+            url: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+            size: new naver.maps.Size(60, 62),
+            origin: new naver.maps.Point(0, 0),
+            anchor: new naver.maps.Point(25, 26)
+        }
+    };
+
+    var infoWindow = new naver.maps.InfoWindow({
+        content: '<div style="width:10px;text-align:center;padding:10px;"><b>' + areaArr[0].location
+    }); // 클릭했을 때 띄워줄 정보 HTML 작성
+
+    markers.push(middlemarker_option); // 생성한 마커를 배열에 담는다.
+    infoWindows.push(infoWindow); // 생성한 정보창을 배열에 담는다.
+
+
+
+    var middlePoint = new naver.maps.Marker(middlemarker_option);
+
+
+    function getClickHandler(seq) {
+
+        return function (e) {  // 마커를 클릭하는 부분
+            var marker = markers[seq], // 클릭한 마커의 시퀀스로 찾는다.
+                infoWindow = infoWindows[seq]; // 클릭한 마커의 시퀀스로 찾는다
+
+            if (infoWindow.getMap()) {
+                infoWindow.close();
+            } else {
+                infoWindow.open(map, marker); // 표출
+            }
+            map.panTo(e.coord); // 마커 클릭시 부드럽게 이동
+        }
+    }
+
+
+    console.log(markers[0], getClickHandler(0));
+    naver.maps.Event.addListener(markers[0], 'click', getClickHandler(0)); // 클릭한 마커 핸들러
+
 }
