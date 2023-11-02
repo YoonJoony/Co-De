be511ab4f703e5df82b4747ec18a5f4314c9c224
ndy <dynam99@naver.com>
2023-11-02 16:59:03 +0900

    ai 채팅

--- src/main/resources/static/js/main_page/main_page.js
+++ src/main/resources/static/js/main_page/main_page.js
@@ -257,11 +257,11 @@ async function fetchAIResponse(prompt) {
           content: prompt, // 사용자가 입력한 메시지
         },
       ],
-      temperature: 0.2, // 모델의 출력 다양성
-      max_tokens: 100, // 응답받을 메시지 최대 토큰(단어) 수 설정
+      temperature: 1, // 모델의 출력 다양성
+      max_tokens: 200, // 응답받을 메시지 최대 토큰(단어) 수 설정
       top_p: 1, // 토큰 샘플링 확률을 설정
-      frequency_penalty: 0.5, // 일반적으로 나오지 않는 단어를 억제하는 정도
-      presence_penalty: 0.5, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
+      frequency_penalty: 0, // 일반적으로 나오지 않는 단어를 억제하는 정도
+      presence_penalty: 0, // 동일한 단어나 구문이 반복되는 것을 억제하는 정도
       stop: ["Human"], // 생성된 텍스트에서 종료 구문을 설정
     }),
   };
