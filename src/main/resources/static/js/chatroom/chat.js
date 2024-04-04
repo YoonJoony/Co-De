6d2d3e49f3ef5b7c61bb34d761e09ec11b1d0870
YoonJoony <dbswns1101@naver.com>
2024-04-04 18:33:26 +0900

    결제 정보에 담을 사용자 정보 API 요청하는 코드 작성

--- src/main/resources/static/js/chatroom/chat.js
+++ src/main/resources/static/js/chatroom/chat.js
@@ -1566,39 +1566,35 @@ function pay_done_btn() {
 }
 
 
+async function fetchUserInfo() {
+  //  서버로부터 사용자 정보를 불러오는 함수
+  const response = await fetch("/members/info");
+  return await response.json();
+}
 
-function requestPay() {
+async function requestPay() {
+  //결제 창 보여주고 결제 정보에 사용자 정보를 담음.
+  const userInfo = await fetchUserInfo();
+
   // 결제 창 호출
   var IMP = window.IMP;
-  IMP.init("imp38136157");
+  IMP.init("imp80525881");
 
   IMP.request_pay(
     {
-      pg: "uplus",
+      pg: "kakaopay",
       pay_method: "card",
       merchant_uid: "test_" + new Date().getTime(),
-      name: "테스트 결제",
-      amount: 100,
-      buyer_tel: "010-0000-0000",
-      buyer_name: "엄준식",
-      buyer_email: "111",
-    },
-    function (rsp) {
+      name: "장바구니 배달음식 결제",
+      amount: 1000,
+      buyer_tel: userInfo.pnum,
+      buyer_name: userInfo.username,
+      buyer_addr : userInfo.address
+    }, async function (rsp) {
       console.log(rsp);
-      // 결제검증
-      $.ajax({
-        type: "POST",
-        url: "/verifyIamport/" + rsp.imp_uid,
-      }).done(function (data) {
-        console.log(data);
-
-        // 위의 rsp.paid_amount 와 data.response.amount를 비교한후 로직 실행 (import 서버검증)
-        if (rsp.paid_amount == data.response.amount) {
-          alert("결제 및 결제검증완료");
-        } else {
-          alert("결제 실패");
-        }
-      });
+      if (rsp.success) {
+        console.log(rsp);
+      }
     }
   );
 }
