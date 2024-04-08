fac8b7695729040e17126b34f8a44a036ed3fa3b
YoonJoony <dbswns1101@naver.com>
2024-04-09 02:13:48 +0900

    사용자 결제 내역을 저장하는 API 생성.
    이후 결제 내역이 기존에 존재할 경우 확인하는 로직도 추가하자.

--- src/main/java/backend/codebackend/controller/PaymentContorller.java
+++ src/main/java/backend/codebackend/controller/PaymentContorller.java
@@ -1,43 +1,51 @@
 package backend.codebackend.controller;
 
+import backend.codebackend.domain.Member;
+import backend.codebackend.domain.Mozip;
 import backend.codebackend.domain.PaymentDetails;
+import backend.codebackend.dto.PaymentDetailsDto;
 import backend.codebackend.service.MemberService;
+import backend.codebackend.service.MozipService;
 import backend.codebackend.service.PaymentsDetailsService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
-import java.util.List;
-
-@Transactional
+@RestController
 @RequiredArgsConstructor
-@Slf4j
 public class PaymentContorller {
     private final PaymentsDetailsService paymentsDetailsService;
     private final MemberService memberService;
+    private final MozipService mozipService;
+    @PostMapping("payment/save")
+    public ResponseEntity<?> payemntDetailsSave(HttpSession session, Long mozipId, String menu, int totalPrice, boolean payStatus){
 
-    @PostMapping("/paymentDetails")
-    public List<PaymentDetails> listPaymentDetailsItems(@PathVariable Long chatroom_id) {
-        List<PaymentDetails> paymentDetailsItems = paymentsDetailsService.findAll(chatroom_id);
-        return paymentDetailsItems;
-    }
-
-    // 사용자 결제 내역 조회
-    @GetMapping("/chat/paymentDetails")
-    @ResponseBody
-    public List<PaymentDetails> paymentDetails(Long payment_id, HttpServletRequest request){
-        HttpSession session = request.getSession(false);
-        List<PaymentDetails> paymentDetails = paymentsDetailsService.findAll(payment_id);
-
-        if (paymentDetails == null) {
-            log.info("결제내역 조회가 되지 않음. ");
+        Optional<Member> member = memberService.findLoginId(String.valueOf(session.getAttribute("memberId")));
+        Optional<Mozip> mozip = mozipService.findRoomById(mozipId);
+        if (member.isEmpty()) {
+            return ResponseEntity.badRequest().body("유저가 조회되지 않습니다.");
         }
-        return paymentDetails;
+        if (mozip.isEmpty()) {
+            return ResponseEntity.badRequest().body("모집글이 조회되지 않습니다");
+        }
+
+        PaymentDetailsDto paymentDetailsDto = PaymentDetailsDto.builder()
+                .nickname(member.get().getNickname())
+                .orderList(menu)
+                .totalPrice(totalPrice)
+                .deliveryAddress(member.get().getAddress())
+                .build();
+
+        if (payStatus) {
+            paymentDetailsDto.setPayStatus(PaymentDetails.PaymentStatus.COMPLETED);
+        }
+        else {
+            paymentDetailsDto.setPayStatus(PaymentDetails.PaymentStatus.FAILED);
+        }
+        paymentsDetailsService.savePayment(paymentDetailsDto, member.get(), mozip.get());
+
+        return ResponseEntity.ok("결제내역이 성공적으로 저장되었습니다.");
     }
 }
