0abc7567001b92d096c1e3b19c27b136a896d86f
YoonJoony <dbswns1101@naver.com>
2024-04-10 17:19:12 +0900

    배달요금 정보 저장을 위한 서비스 및 레포 생성

---
+++ src/main/java/backend/codebackend/repository/DeliveryInfoRepository.java
@@ -0,0 +1,9 @@
+package backend.codebackend.repository;
+
+import backend.codebackend.domain.DeliveryInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeliveryInfoRepository extends JpaRepository<DeliveryInfo, Long> {
+}
---
+++ src/main/java/backend/codebackend/service/DeliveryInfoService.java
@@ -0,0 +1,25 @@
+package backend.codebackend.service;
+
+import backend.codebackend.domain.DeliveryInfo;
+import backend.codebackend.domain.Mozip;
+import backend.codebackend.repository.DeliveryInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeliveryInfoService {
+    private final DeliveryInfoRepository deliveryInfoRepository;
+    public void deliveryInfoSave(Mozip mozip, int deliveryFee, int minFee) {
+        DeliveryInfo deliveryInfo = DeliveryInfo.builder()
+                .mozipId(mozip)
+                .mozipStore(mozip)
+                .deliveryFee(deliveryFee)
+                .minFee(minFee)
+                .build();
+
+        deliveryInfoRepository.save(deliveryInfo);
+    }
+}
