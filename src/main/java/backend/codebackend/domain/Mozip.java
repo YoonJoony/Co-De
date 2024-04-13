b69c0002009fe7e942f18d39c16e5a9b081d04c0
YoonJoony <dbswns1101@naver.com>
2024-04-13 16:46:48 +0900

    DB 컬럼 타입 조건과 JPA @Column 조건 일치시켜야 됨

--- src/main/java/backend/codebackend/domain/Mozip.java
+++ src/main/java/backend/codebackend/domain/Mozip.java
@@ -22,7 +22,7 @@ public class Mozip {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 ID를 자동으로 생성해 주는 전략 : IDENTITY
     private Long id;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 30, nullable = false)
     private String title;
 
     @Column(length = 10, nullable = false)
