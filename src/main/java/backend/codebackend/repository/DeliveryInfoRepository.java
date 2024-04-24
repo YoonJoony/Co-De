e19ab89e9f7c3d9c2fe2e4bbac22a46544f72fe0
YoonJoony <dbswns1101@naver.com>
2024-04-24 14:06:18 +0900

    [수정사항] - 배달비 정보 조회 기능 변화 원래 배달비 정보 조회 법   1. 채팅방 입장 시 메뉴 바로 크롤링 할때 배달지 정보도 크롤링.
    이제 메뉴와 배달비 정보를 방장이 방 생성 시 저장한 DB에서 가져오게 변경 시켰음으로,
    배달비 조회 요청 API를 통해 DB에서 배달비 정보 가져오게 변경

--- src/main/java/backend/codebackend/repository/DeliveryInfoRepository.java
+++ src/main/java/backend/codebackend/repository/DeliveryInfoRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DeliveryInfoRepository extends JpaRepository<DeliveryInfo, Long> {
+    DeliveryInfo findByMozipId_Id(Long mozipId);
 }
